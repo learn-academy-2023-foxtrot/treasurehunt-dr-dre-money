3f5521cbc4cd0218326f36d7451e66085e8591e6
alopezdelgado <andrealdelgado_99@outlook.com>
2023-09-18 11:57:36 -0700

    added alert when clicking question mark, shows index of square on grid

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,14 +1,19 @@
 import React from "react"
 
-const Square = ({board}) => {
+const Square = ({board, handleClick}) => {
   return (
     <>
-    <div className="square"></div>
       <div className="grid">
         {board.map((square, index) => {
           return (
           <div className="square" key={index}>
+            <div 
+              className="square"
+              key={index}
+              onClick={() => handleClick(index)}
+            >
             {square}
+            </div>
           </div>
           )
         })}
